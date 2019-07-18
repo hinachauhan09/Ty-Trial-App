8731bde6a2ff36d26c7049aaf927419f2525d50d
Hina <chauhanhina810@gmail.com>
2019-07-18 23:05:36 +0530

    #some changes

--- app/src/main/java/com/hina/trial/LoginActivity.java
+++ app/src/main/java/com/hina/trial/LoginActivity.java
@@ -58,7 +58,7 @@ public class LoginActivity extends AppCompatActivity {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if (task.isSuccessful()) {
-                                        // Sign in success, update UI with the signed-in user's information
+                                        // Sign in success, update the signed-in user's information
                                         Log.d("user", "signInWithEmail:success");
                                         FirebaseUser user = mAuth.getCurrentUser();
 
